1e46425d970baf1b90cd27d66bf5b363a1de527b
Zahid Allaulddin <zahidalaaudin@gmail.com>
2024-07-20 22:15:52 +0300

    comment out protected index route

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"forum/pkg/db"
 	"forum/pkg/handlers/users"
-	"forum/pkg/middlewares"
 	"forum/pkg/templates"
 	"log"
 	"net/http"
@@ -43,24 +42,24 @@ func main() {
 		users.CreateUserHandler(w, r)
 	})
 
-	// http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-	// 	err := templates.GetTemplate().ExecuteTemplate(w, "index.html", nil)
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 		return
-	// 	}
-	// })
-
-	// An example of using the AuthRequired middleware to protect the index page
-
-	http.Handle("GET /", middlewares.AuthRequired(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		err := templates.GetTemplate().ExecuteTemplate(w, "index.html", nil)
-
 		if err != nil {
 			log.Println(err)
 			return
 		}
-	})))
+	})
+
+	// An example of using the AuthRequired middleware to protect the index page
+
+	// http.Handle("GET /", middlewares.AuthRequired(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	err := templates.GetTemplate().ExecuteTemplate(w, "index.html", nil)
+
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		return
+	// 	}
+	// })))
 
 	log.Println("Connected to the database")
 	log.Println("Server started on port 8080")
